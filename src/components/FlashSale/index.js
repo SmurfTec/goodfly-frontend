8b8be6a58bf7ce650587c8332f967e7f3d54a6aa
UAhmadSoft <umadahmad1928@gmail.com>
2021-09-20 12:51:53 +0500

    Code Formatting

--- src/components/FlashSale/index.js
+++ src/components/FlashSale/index.js
@@ -15,148 +15,168 @@ import TuneIcon from '@material-ui/icons/Tune';
 import ArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import Advisor from './Adivsor';
 
+import { useTheme } from '@material-ui/styles';
+
 const cards = [
-   {
-      title: 'Dubai',
-      _id: '1',
-      desc: 'The Dubai that no one sees',
-      service: 'The GOODFLY guide on site will welcome you ...',
-      noofJourneys: '2 jours',
-      price: '> $200',
-      image: 'https://images.unsplash.com/photo-1583499882110-688e720b025e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZHViYWl8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-   },
-   {
-      title: 'Dubai',
-      _id: '2',
+  {
+    title: 'Dubai',
+    _id: '1',
+    desc: 'The Dubai that no one sees',
+    service: 'The GOODFLY guide on site will welcome you ...',
+    noofJourneys: '2 jours',
+    price: '> $200',
+    image:
+      'https://images.unsplash.com/photo-1583499882110-688e720b025e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZHViYWl8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    title: 'Dubai',
+    _id: '2',
 
-      desc: 'The Dubai that no one sees',
-      service: 'The GOODFLY guide on site will welcome you ...',
-      noofJourneys: '2 jours',
-      price: '> $200',
-      image: 'https://images.unsplash.com/photo-1610823230542-55da5ce635aa?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZHViYWl8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-   },
+    desc: 'The Dubai that no one sees',
+    service: 'The GOODFLY guide on site will welcome you ...',
+    noofJourneys: '2 jours',
+    price: '> $200',
+    image:
+      'https://images.unsplash.com/photo-1610823230542-55da5ce635aa?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZHViYWl8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+  },
 
-   {
-      title: 'Dubai',
-      _id: '3',
+  {
+    title: 'Dubai',
+    _id: '3',
 
-      desc: 'The Dubai that no one sees',
-      service: 'The GOODFLY guide on site will welcome you ...',
-      noofJourneys: '2 jours',
-      price: '> $200',
-      image: 'https://images.unsplash.com/photo-1589695021834-9f2413573b28?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8ZHViYWl8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-   },
-   {
-      title: 'Dubai',
-      _id: '4',
-      desc: 'The Dubai that no one sees',
-      service: 'The GOODFLY guide on site will welcome you ...',
-      noofJourneys: '2 jours',
-      price: '> $2000',
-      image: 'https://images.unsplash.com/photo-1610823230542-55da5ce635aa?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZHViYWl8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-   },
+    desc: 'The Dubai that no one sees',
+    service: 'The GOODFLY guide on site will welcome you ...',
+    noofJourneys: '2 jours',
+    price: '> $200',
+    image:
+      'https://images.unsplash.com/photo-1589695021834-9f2413573b28?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8ZHViYWl8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    title: 'Dubai',
+    _id: '4',
+    desc: 'The Dubai that no one sees',
+    service: 'The GOODFLY guide on site will welcome you ...',
+    noofJourneys: '2 jours',
+    price: '> $2000',
+    image:
+      'https://images.unsplash.com/photo-1610823230542-55da5ce635aa?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZHViYWl8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+  },
 ];
 
 const options = ['Price', 'Date', 'Duration', 'Best Score'];
 
-const FlashSale = ({ history }) => {
-   const classes = styles();
+const FlashSale = ({ location }) => {
+  const theme = useTheme();
 
-   //? Filter Menu State
-   const [anchorEl, setAnchorEl] = React.useState(null);
-   const open = Boolean(anchorEl);
+  const classes = styles({ location: location, theme: theme });
 
-   //? Closing filter menu
-   const handleClose = () => {
-      setAnchorEl(null);
-   };
+  //? Filter Menu State
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
 
-   // ? Filter Menu open
-   const filterMenuOpen = (e) => {
-      setAnchorEl(e.currentTarget);
-   };
+  //? Closing filter menu
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-   //? Filter Item selected
-   const filterSelected = (e) => {
-      //? Got the selected filter value, uncomment below line
-      //   const { filter } = e.currentTarget.dataset;
-      //   console.log(filter);
+  // ? Filter Menu open
+  const filterMenuOpen = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
 
-      setAnchorEl(null);
-   };
+  //? Filter Item selected
+  const filterSelected = (e) => {
+    //? Got the selected filter value, uncomment below line
+    //   const { filter } = e.currentTarget.dataset;
+    //   console.log(filter);
 
-   return (
-      <React.Fragment>
-         <CssBaseline />
+    setAnchorEl(null);
+  };
 
-         <main className={classes.root}>
-            {/* Hero unit */}
-            <Container className={classes.cardGrid} maxWidth='lg'>
-               <div className={classes.heroContent}>
-                  <Container className={classes.mainFeaturedPost}>
-                     <section className={classes.title}>
-                        <Typography variant='h3'>
-                           FLASH SALE
-                           <FlashOnIcon sx={{ marginLeft: 2 }} />
-                        </Typography>
-                     </section>
-                     <Advisor />
-                  </Container>
+  return (
+    <React.Fragment>
+      <CssBaseline />
 
-                  <section className={classes.filter}>
-                     <Button
-                        variant='outlined'
-                        startIcon={<TuneIcon />}
-                        onClick={filterMenuOpen}
-                     >
-                        Select a filter
-                     </Button>
-                     <Menu
-                        id='long-menu'
-                        anchorEl={anchorEl}
-                        keepMounted
-                        open={open}
-                        onClose={handleClose}
-                     >
-                        {options.map((option, index) => (
-                           <MenuItem
-                              key={option}
-                              data-filter={option}
-                              onClick={filterSelected}
-                           >
-                              {option}
-                           </MenuItem>
-                        ))}
-                     </Menu>
-                  </section>
-               </div>
-               {/* End hero unit */}
-
-               {/* Upper GridView */}
-               <Grid container spacing={4}>
-                  {cards.map((card) => (
-                     <Grid item key={card._id} xs={12} sm={6} md={4}>
-                        <FlashCard {...card} />
-                     </Grid>
-                  ))}
-               </Grid>
-
-               {/* Space Container */}
-               <div className={classes.spaceSection}>
-                  <Typography variant='h5'>PUB SPACE</Typography>
-               </div>
-
-               {/* Lower GridView */}
-               <Grid container spacing={4}>
-                  {cards.map((card) => (
-                     <Grid item key={card._id} xs={12} sm={6} md={4}>
-                        <FlashCard {...card} />
-                     </Grid>
-                  ))}
-               </Grid>
+      <main className={classes.root}>
+        {/* Hero unit */}
+        <Container className={classes.cardGrid} maxWidth='lg'>
+          <div className={classes.heroContent}>
+            <Container className={classes.mainFeaturedPost}>
+              <section className={classes.title}>
+                <Typography variant='h3'>
+                  FLASH SALE
+                  <FlashOnIcon sx={{ marginLeft: 2 }} />
+                </Typography>
+              </section>
+              <Advisor />
             </Container>
-         </main>
-      </React.Fragment>
-   );
+
+            <section className={classes.filter}>
+              <Button
+                variant='outlined'
+                startIcon={<TuneIcon />}
+                onClick={filterMenuOpen}
+              >
+                Select a filter
+              </Button>
+              <Menu
+                id='long-menu'
+                anchorEl={anchorEl}
+                keepMounted
+                open={open}
+                onClose={handleClose}
+              >
+                {options.map((option, index) => (
+                  <MenuItem
+                    key={option}
+                    data-filter={option}
+                    onClick={filterSelected}
+                  >
+                    {option}
+                  </MenuItem>
+                ))}
+              </Menu>
+            </section>
+          </div>
+          {/* End hero unit */}
+          <Typography variant='h5' color='textSecondary' align='left'>
+            Do you have a few days ahead of you? Découvrez les ventes
+          </Typography>
+          <Typography
+            variant='h5'
+            color='textSecondary'
+            align='left'
+            style={{
+              marginBottom: '2rem',
+            }}
+          >
+            Flash GOODFLY : les plans dernière minute à prix cassés.
+          </Typography>
+          {/* Upper GridView */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card._id} xs={12} sm={6} md={4}>
+                <FlashCard {...card} />
+              </Grid>
+            ))}
+          </Grid>
+
+          {/* Space Container */}
+          <div className={classes.spaceSection}>
+            <Typography variant='h5'>PUB SPACE</Typography>
+          </div>
+
+          {/* Lower GridView */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card._id} xs={12} sm={6} md={4}>
+                <FlashCard {...card} />
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+    </React.Fragment>
+  );
 };
 export default withRouter(FlashSale);
