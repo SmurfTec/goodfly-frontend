c8fe0bd03edeec8afce1e0adb126101162b03acd
UAhmadSoft <umadahmad1928@gmail.com>
2021-10-22 22:10:50 +0500

    Added Hoverable Dropdown to Navbar

---
+++ src/components/common/NavbarData.js
@@ -0,0 +1,51 @@
+const NavbarData = [
+  {
+    path: '/tours/ethical',
+    title: 'Ethical Trips',
+    menuItems: ['organic', 'organized'],
+  },
+  {
+    path: '/tours/spiritual',
+    title: 'Spiritual Trips',
+    menuItems: ['hajj', 'omra', 'combine-hajj-omra', 'al-quds'],
+  },
+  {
+    path: '/tours/excursions',
+    title: 'Excursions & Circuits',
+    menuItems: ['excursions', 'circuits'],
+  },
+  {
+    path: '/tours/destinations',
+    title: 'Destinations',
+  },
+  {
+    path: '/ticketing',
+    title: 'Ticketing',
+  },
+  {
+    path: '/hotels',
+    title: 'Hotels and accommodations',
+  },
+  {
+    path: '/tours/flash-sales',
+    title: ' Flash Sales',
+  },
+  {
+    path: '/blogs',
+    title: 'Blog',
+  },
+  {
+    path: '/promos',
+    title: 'Promos',
+  },
+  {
+    path: '/tours/create',
+    title: 'Create Trip',
+  },
+  {
+    path: '/store',
+    title: 'Store',
+  },
+];
+
+export default NavbarData;
