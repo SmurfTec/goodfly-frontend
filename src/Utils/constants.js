bb64d0a1cd10ce4fda9fd13e07765b60008a93b7
muhammadzain8 <muhammadzain8@gmail.com>
2021-11-16 22:24:30 +0500

    url changes

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -1,10 +1,10 @@
 import { toast } from 'react-toastify';
 
 // * Development URLs
-const API_BASE_URL = `http://localhost:7000/api`;
+// const API_BASE_URL = `http://localhost:7000/api`;
 
 // * Production URLs
-// const API_BASE_URL = `https://goodfly-api.herokuapp.com/api`;
+const API_BASE_URL = `https://goodfly-api.herokuapp.com/api`;
 // const API_BASE_URL = `http://d998-119-73-113-182.ngrok.io/api`;
 
 const handleCatch = (err) => {
@@ -15,7 +15,11 @@ const handleCatch = (err) => {
   toast.error(errMsg);
 };
 
-const makeReq = (endpoint, { body, ...customConfig } = {}, method = 'GET') => {
+const makeReq = (
+  endpoint,
+  { body, ...customConfig } = {},
+  method = 'GET'
+) => {
   const token = localStorage.getItem('jwt');
   const headers = { 'Content-Type': 'application/json' };
 
@@ -38,15 +42,17 @@ const makeReq = (endpoint, { body, ...customConfig } = {}, method = 'GET') => {
   }
 
   // console.log(`body`, body);
-  return fetch(`${API_BASE_URL}${endpoint}`, config).then(async (res) => {
-    const data = await res.json();
-    // console.log(`data`, data);
-    if (res.ok) {
-      return data;
-    } else {
-      return Promise.reject(data);
+  return fetch(`${API_BASE_URL}${endpoint}`, config).then(
+    async (res) => {
+      const data = await res.json();
+      // console.log(`data`, data);
+      if (res.ok) {
+        return data;
+      } else {
+        return Promise.reject(data);
+      }
     }
-  });
+  );
 };
 
 const countryCodes = [
